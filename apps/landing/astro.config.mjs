597cc97b8a8073aa0318c870378d1d58fcd81ea4
odimsom <20231065@itla.edu.do>
2025-07-24 01:21:14 -0400

    .

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    react(),
+  ],
   vite: {
     ssr: {
       noExternal: ["lucide-react"],
--- apps/landing/astro.config.vercel.mjs
+++ apps/landing/astro.config.vercel.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    react(),
+  ],
   vite: {
     ssr: {
       noExternal: ["lucide-react"],
